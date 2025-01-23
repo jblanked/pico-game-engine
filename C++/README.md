5b3602cba21e859707de244e857f1ac64b13cd5a
jblanked <82678820+jblanked@users.noreply.github.com>
2025-01-23 14:00:13 -0500

    add library properties and simplify

---
+++ C++/README.md
@@ -0,0 +1,4 @@
+## Installation
+1. Download the repository as a `.zip` file.
+2. Unzip the file, then copy the entire `C++` folder and paste it in `Arduino -> libraries`.
+3. Add `#include "PicoGameEngine.h"` to the top of your `.ino`.
---
+++ C++/pico_game_engine/PicoGameEngine.h
@@ -0,0 +1,34 @@
+#pragma once
+#include "vector.h"
+#include "entity.h"
+#include "level.h"
+#include "input.h"
+#include "image.h"
+#include "draw.h"
+#include "game.h"
+#include "engine.h"
+/*
+Pico Game Engine
+Author: JBlanked
+Github: https://github.com/jblanked/Pico-Game-Engine
+Info: A simple game engine for the Raspberry Pi Pico, TFT display, and buttons.
+Created: 2025-01-21
+Updated: 2025-01-23
+
+Wiring (TFT -> Pico):
+- GNDD -> GND
+- VCC -> VSYS (Pin 39)
+- CS -> GP9 (Pin 12)
+- RESET -> GP10 (Pin 14)
+- DC -> GP11 (Pin 15)
+- SDI/MOSI -> GP6 (Pin 9)
+- SCK -> GP7 (Pin 10)
+- LED -> VSYS (Pin 39)
+
+Button mapping (used as D-pad for now since we have no A, B, X, Y buttons)
+# Controller -> Pico
+- Yellow (UP) -> GP16 (Pin 21 - RX)
+- Red (RIGHT) -> GP17 (Pin 22 - TX)
+- Green (DOWN) -> GP18 (Pin 25)
+- Blue (LEFT) -> GP19 (Pin 26)
+*/
